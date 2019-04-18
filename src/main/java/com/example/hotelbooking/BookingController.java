e1a4e7b0ee712c57a79fde152dfafbdc05eb7ff4
Daniel Obembe <obembe.dan@gmail.com>
2019-04-18 10:03:38 -0500

    Introduced DB

--- src/main/java/com/example/hotelbooking/BookingController.java
+++ src/main/java/com/example/hotelbooking/BookingController.java
@@ -1,5 +1,6 @@
 package com.example.hotelbooking;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,31 +10,33 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping(value="/bookings")
 public class BookingController {
-    private List<HotelBooking> bookings;
 
-    public BookingController(){
-        bookings = new ArrayList<>();
+    BookingRepository bookingRepository;
 
-        bookings.add(new HotelBooking("Marriot", 200.50, 3));
-        bookings.add(new HotelBooking("Ibis", 90, 4));
-        bookings.add(new HotelBooking("Novotel", 140.74, 1));
+    @Autowired
+    public BookingController(BookingRepository bookingRepository){
+        this.bookingRepository = bookingRepository;
     }
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public List<HotelBooking> getAll() {
-        return bookings;
+        return bookingRepository.findAll();
     }
 
     @RequestMapping(value = "/affordable/{price}", method = RequestMethod.GET)
     public List<HotelBooking> getAffordable(@PathVariable double price) {
-        return bookings.stream().filter(x -> x.getPricePerNight() <= price)
-                .collect(Collectors.toList());
+        return bookingRepository.findByPricePerNightLessThan(price);
     }
 
     @RequestMapping(value="/create", method = RequestMethod.POST)
     public List<HotelBooking> create(@RequestBody HotelBooking hotelBooking) {
-        bookings.add(hotelBooking);
+        bookingRepository.save(hotelBooking);
+        return bookingRepository.findAll();
+    }
 
-        return bookings;
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public List<HotelBooking> remove(@PathVariable long id) {
+        bookingRepository.deleteById(id);
+        return bookingRepository.findAll();
     }
 }
--- src/main/java/com/example/hotelbooking/HotelBooking.java
+++ src/main/java/com/example/hotelbooking/HotelBooking.java
@@ -1,6 +1,17 @@
 package com.example.hotelbooking;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class HotelBooking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
     private String hotelName;
     private double pricePerNight;
     private int nbOfNights;
@@ -27,5 +38,9 @@ public class HotelBooking {
 
     public double getTotalPrice() {
         return pricePerNight * nbOfNights;
+    }
+
+    public long getId() {
+        return id;
     }
 }
